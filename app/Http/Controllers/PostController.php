dce9eab4a50ba1e8e5de8b6c2bfcbf6e050a4634
sailirizkizahara <sailirizki03@gmail.com>
2025-11-14 22:54:25 +0700

    Update: Menambahkan galeri foto kampus di halaman post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,16 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View; // tambahkan ini
+
 class PostController extends Controller
 {
     public function index()
     {
-        // Dummy data postingan
         $posts = [
-            ['title' => 'Universitas Bumigora', 'body' => 'Terkemuka di bidang komputer di NTB.'],
-            ['title' => 'Fakultas Ilmu Komputer', 'body' => 'Mencetak mahasiswa berprestasi.'],
+            ['title' => 'Post 1', 'body' => 'Isi post 1'],
+            ['title' => 'Post 2', 'body' => 'Isi post 2'],
+            ['title' => 'Post 3', 'body' => 'Isi post 3'],
+            ['title' => 'Post 4', 'body' => 'Isi post 4'],
+            ['title' => 'Post 5', 'body' => 'Isi post 5'],
+            ['title' => 'Post 6', 'body' => 'Isi post 6'],
         ];
 
-        return view('post', compact('posts'));
+        $profiles = [
+            [
+                'name' => 'GEDUNG UNIVERSITAS BUMIGORA',
+                'desc' => 'MNDHBCOSUDDFC',
+                'img'  => 'images/ubgg.jpg',
+            ],
+            [
+                'name' => 'GEDUNG UNIVERSITAS ISLAM NEGERI',
+                'desc' => 'AAAAAAAAAAAAAAAAAAAAAAAA',
+                'img'  => 'images/UIN.jpg',
+            ],
+            [
+                'name' => 'GEDUNG UNDIKMA',
+                'desc' => 'AAAAAAAAAAAAAAAAAAAAAAAA',
+                'img'  => 'images/undikma.jpg',
+            ],
+            [
+                'name' => 'GEDUNG UNIZAR',
+                'desc' => 'AAAAAAAAAAAAAAAAAAAAAAAA',
+                'img'  => 'images/unizar.webp',
+            ],
+            [
+                'name' => 'GEDUNG UNIVERSITAS MATARAM',
+                'desc' => 'AAAAAAAAAAAAAAAAAAAAAAAA',
+                'img'  => 'images/unram.jpg',
+            ],
+            [
+                'name' => 'GEDUNG UNW',
+                'desc' => 'AAAAAAAAAAAAAAAAAAAAAAAA',
+                'img'  => 'images/unw.jpg',
+            ],
+        ];
+
+        return view('post', compact('posts', 'profiles'));
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,10 +3,97 @@
 @section('content')
 <h1>Daftar Postingan</h1>
 
+{{-- LIST POSTINGAN --}}
 @foreach($posts as $post)
     <h3>{{ $post['title'] }}</h3>
     <p>{{ $post['body'] }}</p>
     <hr>
 @endforeach
+
+
+{{-- CARD PROFILE --}}
+<div class="card" style="width: 20rem;">
+    <img class="card-img-top" 
+         src="images/ubgg.jpg" 
+         alt="Gedung UNIVERSITAS BUMIGORA">
+    
+    <div class="card-body">
+        <h5 class="card-title">Gedung UNIVERSITAS BUMIGORA</h5>
+        <p class="card-text">
+            Foto Gedung UBG.
+        </p>
+        <a href="#" class="btn btn-primary">Lihat Detail</a>
+    </div>
+</div>
+
+<div class="card" style="width: 20rem;">
+    <img class="card-img-top" 
+         src="images/UIN.jpg" 
+         alt="Gedung UNIVERSITAS ISLAM NEGERI">
+    
+    <div class="card-body">
+        <h5 class="card-title">Gedung UNIVERSITAS ISLAM NEGERI</h5>
+        <p class="card-text">
+            Foto Gedung UIN.
+        </p>
+        <a href="#" class="btn btn-primary">Lihat Detail</a>
+    </div>
+</div>
+
+<div class="card" style="width: 20rem;">
+    <img class="card-img-top" 
+         src="images/undikma.jpg" 
+         alt="Gedung UNDIKMA">
+    
+    <div class="card-body">
+        <h5 class="card-title">Gedung UNDIKMA</h5>
+        <p class="card-text">
+            Foto Gedung UNDIKMA.
+        </p>
+        <a href="#" class="btn btn-primary">Lihat Detail</a>
+    </div>
+</div>
+
+<div class="card" style="width: 20rem;">
+    <img class="card-img-top" 
+         src="images/unizar.webp" 
+         alt="Gedung UNIZAR">
+    
+    <div class="card-body">
+        <h5 class="card-title">Gedung UNIZAR</h5>
+        <p class="card-text">
+            Foto Gedung UNIZAR.
+        </p>
+        <a href="#" class="btn btn-primary">Lihat Detail</a>
+    </div>
+</div>
+
+<div class="card" style="width: 20rem;">
+    <img class="card-img-top" 
+         src="images/unram.jpeg" 
+         alt="Gedung Rektorat Universitas Mataram">
+    
+    <div class="card-body">
+        <h5 class="card-title">Rektorat Universitas Mataram</h5>
+        <p class="card-text">
+            Foto Gedung Rektorat UNRAM.
+        </p>
+        <a href="#" class="btn btn-primary">Lihat Detail</a>
+    </div>
+</div>
+
+<div class="card" style="width: 20rem;">
+    <img class="card-img-top" 
+         src="images/unw.jpg" 
+         alt="Gedung UNW">
+    
+    <div class="card-body">
+        <h5 class="card-title">Gedung UNW</h5>
+        <p class="card-text">
+            Foto Gedung UNW.
+        </p>
+        <a href="#" class="btn btn-primary">Lihat Detail</a>
+    </div>
+</div>
 
 @endsection
